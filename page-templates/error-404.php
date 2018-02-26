fa6609fa588d6cc6c2f08c3117bfed578ede95d2
Ben Davidson <ben@bendavidson.me>
2018-02-26 16:13:07 -0500

    Updated 404 template

--- page-templates/error-404.php
+++ page-templates/error-404.php
@@ -7,7 +7,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
-  <?php get_partial('partials/meta/head'); ?>
+  <?php get_partial('meta/head'); ?>
 
   <body <?php body_class('error error--404'); ?>>
 
@@ -28,26 +28,26 @@
     <footer class="universal-footer">
       <?php // Universal footer blocks
         // Newsletter signup form
-        get_partial('partials/footer/newsletter');
+        get_partial('footer/newsletter');
 
         // Footer masthead
-        get_partial('partials/footer/masthead');
+        get_partial('footer/masthead');
 
         // Quicklink buttons
-        get_partial('partials/footer/quicklinks');
+        get_partial('footer/quicklinks');
 
         // Contact blocks
-        get_partial('partials/footer/contacts');
+        get_partial('footer/contacts');
 
         // Crediibility badges
-        get_partial('partials/footer/badges');
+        get_partial('footer/badges');
 
         // Copyright phrasing and links
-        get_partial('partials/footer/copyright');
+        get_partial('footer/copyright');
       ?>
     </footer>
 
-    <?php get_partial('partials/meta/foot'); ?>
+    <?php get_partial('meta/foot'); ?>
 
   </body>
 
