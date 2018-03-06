f060227de9a79e58e449e89373926f1472fd8373
Ben Davidson <ben@bendavidson.me>
2018-03-06 13:41:30 -0500

    Removed WP-included jQuery, updated included scripts calling

--- setup/assets.php
+++ setup/assets.php
@@ -2,10 +2,13 @@
 // Theme assets to load
 function bellhop_asset_loader()
 {
+  wp_deregister_script('jquery');
+  wp_deregister_script('jquery-migrate');
+
   // Styles
-  wp_enqueue_style('main/css', get_stylesheet_directory_uri() . '/assets/styles/dist/main.min.css', false, null);
+  wp_enqueue_style('main', get_stylesheet_directory_uri() . '/assets/styles/dist/main.min.css', false, null);
 
   // Scripts
-  wp_enqueue_script('index/js', get_stylesheet_directory_uri() . '/assets/scripts/dist/index.min.js', array('jquery'), null, true);
+  wp_enqueue_script('index', get_stylesheet_directory_uri() . '/assets/scripts/dist/index.min.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'bellhop_asset_loader', 100);
