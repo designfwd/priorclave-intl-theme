e6789ff1037f5afb0f45003a36cf91aa71b8aea3
Ben Davidson <ben@bendavidson.me>
2018-03-07 11:45:19 -0500

    Updated layout markup for Link Blocks module

--- partials/navigation/link-blocks.php
+++ partials/navigation/link-blocks.php
@@ -1,3 +1,23 @@
 <?php
 // Transparent block links with background imagery
 ?>
+<section class="o-linkBlocks" style="background-image: url('<?php get_picsum(2400,900,'grayscale'); ?>')">
+  <h2 class="o-linkBlocks__headline">How Can We Help You?</h2>
+  <div class=o-linkedBlocks__links>
+    <?php // Imitating the repeater likely to come from ACF
+      $buttonText = array(
+        'Find a Distributor',
+        'Request a Quote',
+        'Request Support'
+      );
+      foreach( $buttonText as $text ):
+    ?>
+      <a class="m-blockLink" href="#">
+        <img class="m-blockLink__icon lazyload" src="<?php placeholder_img( 90, 90, 'text=icon'); ?>" />
+        <h3 class="m-blockLink__text"><?php echo $text; ?></h3>
+      </a>
+    <?php
+      endforeach;
+    ?>
+  </div>
+</section>
