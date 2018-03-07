6df85e5ddb3f43051e8abf2654adba150506c211
Ben Davidson <ben@bendavidson.me>
2018-03-07 13:29:40 -0500

    Updated intro content layout and styles

--- partials/content/intro-decorated.php
+++ partials/content/intro-decorated.php
@@ -1,3 +1,12 @@
 <?php
 // Introductory content block with a headline and body content, followed by a visual decoration
 ?>
+<section class="o-introContent">
+  <h1 class="o-introContent__headline">Leader in Steam Autoclaves</h1>
+  <div class="o-introContent__content">
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+    </p>
+  </div>
+  <img class="o-introContent__icon" src="<?php placeholder_img(139,76, 'text=bee icon'); ?>" />
+</section>
