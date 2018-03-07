2b1994c77016880d5b448e35dd45bc8b0a3ac8f5
Ben Davidson <ben@bendavidson.me>
2018-03-07 15:06:35 -0500

    Added multiregion navigation layout

--- partials/navigation/regional.php
+++ partials/navigation/regional.php
@@ -1,3 +1,54 @@
 <?php
 // Regional navigation for use to link to a particular country or region
 ?>
+<nav id="multiregion-homepage-navigation">
+
+  <?php // Rough multiregion navigation structure, mimicing WP div structure ?>
+  <div>
+    <ul class="o-regionalNav">
+      <li class="menu-item-depth-0 menu-item">
+        <a class="menu-link" href="#">Africa</a>
+      </li>
+      <li class="menu-item-depth-0 menu-item menu-item-has-children">
+        <a class="menu-link" href="#">Asia Pacific</a>
+        <ul class="sub-menu menu-depth-1">
+          <li class="sub-menu-item menu-item-depth-1 menu-item">
+            <a class="menu-link sub-menu-link" href="#">Australia</a>
+          </li>
+          <li class="sub-menu-item menu-item-depth-1 menu-item">
+            <a class="menu-link sub-menu-link" href="#">Malaysia</a>
+          </li>
+          <li class="sub-menu-item menu-item-depth-1 menu-item">
+            <a class="menu-link sub-menu-link" href="#">Philippines</a>
+          </li>
+        </ul>
+      </li>
+      <li class="menu-item-depth-0 menu-item menu-item-has-children">
+        <a class="menu-link" href="#">Europe</a>
+        <ul class="sub-menu menu-depth-1">
+          <li class="sub-menu-item menu-item-depth-1 menu-item">
+            <a class="menu-link sub-menu-link" href="#">United Kingdom</a>
+          </li>
+        </ul>
+      </li>
+      <li class="menu-item-depth-0 menu-item">
+        <a class="menu-link" href="#">Middle East</a>
+      </li>
+      <li class="menu-item-depth-0 menu-item menu-item-has-children">
+        <a class="menu-link" href="#">North America</a>
+        <ul class="sub-menu menu-depth-1">
+          <li class="sub-menu-item menu-item-depth-1 menu-item">
+            <a class="menu-link sub-menu-link" href="#">Canada</a>
+          </li>
+          <li class="sub-menu-item menu-item-depth-1 menu-item">
+            <a class="menu-link sub-menu-link" href="#">United States</a>
+          </li>
+        </ul>
+      </li>
+      <li class="menu-item-depth-0 menu-item">
+        <a class="menu-link" href="#">South America</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
