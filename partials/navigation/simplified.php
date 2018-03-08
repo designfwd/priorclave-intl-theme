85fb2935d0449ddec569b3e582ccc43cd72bf966
Ben Davidson <ben@bendavidson.me>
2018-03-08 11:07:34 -0500

    Updated markup and styles for simple nav

--- partials/navigation/simplified.php
+++ partials/navigation/simplified.php
@@ -2,7 +2,7 @@
 // Navigation to use in lieu of primary nav on international homepages
 ?>
 <section class="o-simpleNav">
-  <img class="o-simpleNav__logo lazyload lazyload--blurUp" src="<?php placeholder_img( 384, 152, 'text=logo'); ?>" />
+  <img class="o-simpleNav__logo lazyload lazyload--blurUp" src="<?php placeholder_img( 192, 76, 'text=logo'); ?>" />
   <div class="o-simpleNav__button">
     <a class="a-navButton" href="#">Contact</a>
   </div>
