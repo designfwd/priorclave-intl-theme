5074aa3af60d4f592e2a9b3f5c78154ca654faf0
Ben Davidson <ben@bendavidson.me>
2018-03-09 17:07:23 -0500

    Updated styles for CTA Background module

--- partials/media/cta-background.php
+++ partials/media/cta-background.php
@@ -1,3 +1,12 @@
 <?php
 // Call-to-action block with a prominent background image
 ?>
+<section class="o-ctaBackground" style="background-image:url('<?php get_picsum(2400,1400,'grayscale'); ?>')">
+  <h2 class="o-ctaBackground__headline">We're Here to Help</h2>
+  <div class="o-ctaBackground__description">
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+    </p>
+  </div>
+  <a class="o-ctaBackground__button" href="#">Service & Maintenance</a>
+</section>
