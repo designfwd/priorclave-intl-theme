b2761babdae65d17f9f2b0349dd1409014b12794
Ben Davidson <ben@bendavidson.me>
2018-03-12 17:00:35 -0400

    Added markup and styles for product grid module

--- partials/navigation/product-grid.php
+++ partials/navigation/product-grid.php
@@ -1,3 +1,29 @@
 <?php
 // Grid of linked product images
 ?>
+<section class="o-productGrid">
+  <h2 class="o-productGrid__headline">Steam Autoclaves</h2>
+  <div class="o-productGrid__products">
+    <?php // Products for the grid
+      $products = array(
+        "Top-Loading/Vertical Autoclaves",
+        "Front-Loading Autoclaves",
+        "Benchtop Autoclaves",
+        "Rectangular Autoclaves",
+        "Power Door Autoclaves",
+        "Double-Ended/Pass-Through Autoclaves",
+        "Horizontal Autoclaves",
+        "Large Capacity Autoclaves",
+        "Custom Autoclaves"
+      );
+      foreach( $products as $product ):
+    ?>
+      <a class="m-productGridItem" href="#">
+        <img class="m-productGridItem__image" src="<?php placeholder_img(rand(220,280),rand(340,400),'text=Autoclave'); ?>" />
+        <h3 class="m-productGridItem__title"><?php echo $product; ?></h3>
+      </a>
+    <?php
+      endforeach;
+    ?>
+  </div>
+</section>
