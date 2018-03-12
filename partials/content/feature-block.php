e51d5813a5f9db05aa9a9c4292af8740b7ab2327
Ben Davidson <ben@bendavidson.me>
2018-03-12 16:31:35 -0400

    Updated layout and styles for simple CTA

--- partials/content/feature-block.php
+++ partials/content/feature-block.php
@@ -1,3 +1,11 @@
 <?php
 // A block of content featured after hero and introductory content
 ?>
+<section class="o-featureBlock">
+  <h2 class="o-featureBlock__headline">Autoclaves for Your Industry</h2>
+  <div class="o-featureBlock__content">
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+    </p>
+  </div>
+</section>
--- partials/media/cta-simple.php
+++ partials/media/cta-simple.php
@@ -1,3 +1,12 @@
 <?php
 // Simple call-to-action with a headline, body, and page-linked button
 ?>
+<section class="o-ctaSimple">
+  <h2 class="o-ctaSimple__headline">Get Pricing for Your Perfect Autoclave Fast</h2>
+  <div class="o-ctaSimple__description">
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
+    </p>
+  </div>
+  <a class="o-ctaSimple__button" href="#">Request a Quote</a>
+</section>
