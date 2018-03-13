01e6f82c2a8e6806f6280b537a083e51cf1f1a62
Ben Davidson <ben@bendavidson.me>
2018-03-13 13:52:48 -0400

    Updated layout and styles for marketing hero module

--- partials/content/intro-block.php
+++ partials/content/intro-block.php
@@ -1,3 +1,11 @@
 <?php
 // Introductory content block with a headline and body content
 ?>
+<section class="o-introContent">
+  <h1 class="o-introContent__headline">Leader in Steam Autoclaves</h1>
+  <div class="o-introContent__content">
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
+    </p>
+  </div>
+</section>
--- partials/hero/featured-marketing.php
+++ partials/hero/featured-marketing.php
@@ -1,3 +1,36 @@
 <?php
 // Single-slide marketing message with image and optional distributor contact capability
 ?>
+<section class="o-heroMarketing" style="background-image:url('<?php get_picsum(2550,1660,'grayscale'); ?>')">
+  <h1 class="o-heroMarketing__content">
+    <?php // Lines of text
+      $textLines = array(
+        'Reliable.',
+        'Flexible.',
+        'Programmable.',
+        'Affordable.',
+        'Autoclaves',
+      );
+      for( $i=0; $i<count($textLines); $i++):
+        // Outputs a different style for the final line of text
+        if( $i == (count($textLines)-1) ):
+      ?>
+        <span class="o-heroMarketing__content--feature">
+          <?php echo $textLines[$i];?>
+          <br />
+        </span>
+      <?php
+        else:
+          echo $textLines[$i];
+        ?>
+          <br />
+      <?php
+        endif;
+      endfor;
+    ?>
+    <div class="o-heroMarketing__content--subtitle">
+      The Right Autoclave for Your Industry
+    </div>
+  </h1>
+  <img class="o-heroMarketing__image" src="<?php placeholder_img(650,460,'text=Marketing Image'); ?>" />
+</section>
