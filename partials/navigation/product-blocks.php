2f8fd8dc364df915423911d5412fb0a3bed4a51b
Ben Davidson <ben@bendavidson.me>
2018-03-14 16:44:14 -0400

    Updated markup and styles for product navigation module

--- partials/navigation/product-blocks.php
+++ partials/navigation/product-blocks.php
@@ -1,3 +1,9 @@
 <?php
-// Intra-page navigation for a particular produc,t arranged in blocks
+// Intra-page navigation for a particular product, arranged in blocks
 ?>
+<section class="o-productNavigation">
+  <a class="o-productNavigation__link" href="#productFeatures">Features</a>
+  <a class="o-productNavigation__link" href="#productOptions">Options</a>
+  <a class="o-productNavigation__link" href="#productSpecifications">Specifications</a>
+  <a class="o-productNavigation__link" href="#productDownloads">Downloads</a>
+</section>
