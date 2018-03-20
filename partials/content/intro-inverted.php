c9fe19617dabf800bfd71dcb6f03c9565fe7ee5f
Ben Davidson <ben@bendavidson.me>
2018-03-20 16:25:39 -0400

    Updated layout and styles for inverted intro content module

--- partials/content/intro-inverted.php
+++ partials/content/intro-inverted.php
@@ -1,3 +1,11 @@
 <?php
 // Introductory content block with a headline and body content, with inverted colors from the primary
 ?>
+<section class="o-introContent o-introContent--inverted">
+  <h1 class="o-introContent__headline">Leader in Steam Autoclaves</h1>
+  <div class="o-introContent__content">
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+    </p>
+  </div>
+</section>
