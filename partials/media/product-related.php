02c3b7fb679f21a3b197d89f0970b814fa129d0f
Ben Davidson <ben@bendavidson.me>
2018-03-20 17:16:02 -0400

    Updated styles for related product module

--- partials/media/product-related.php
+++ partials/media/product-related.php
@@ -1,3 +1,24 @@
 <?php
 // Content block to promote a particular product or cateogry, with linked button
 ?>
+<section class="o-relatedProduct">
+  <h2 class="o-relatedProduct__headline">
+    Priorclave Steam Autoclaves
+  </h2>
+  <div class="o-relatedProduct__content">
+    <div class="m-relatedContent">
+      <div class="m-relatedContent__body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <div class="m-relatedContent__button">
+        <a class="a-bigButton" href="#">View Autoclaves</a>
+      </div>
+    </div>
+    <img class="m-relatedContent m-relatedContent--image lazyload" src="<?php get_picsum(335,500); ?>" />
+  </div>
+</section>
