455eade7e4ead0fe0fb3f60d1453c02f2030b18e
Ben Davidson <ben@bendavidson.me>
2018-03-21 15:50:10 -0400

    Updated layout and styles for sidebar widgets

--- partials/widget/links-related.php
+++ partials/widget/links-related.php
@@ -1,3 +1,26 @@
 <?php
 // Sidebar widget to display a list of page links
 ?>
+<section class="o-widget">
+  <h2 class="o-widget__headline">Related Pages</h2>
+  <?php
+    // List of links as placeholder
+    $links = array(
+      'Excellent Customer Service',
+      'Unique Features',
+      'Quality, Reliability, & Safety',
+      'Energy & Water Efficiency',
+      'Accreditation & Certification',
+      'Customers & Case Studies',
+      'News',
+      'About Priorclave'
+    );
+    foreach( $links as $link ):
+  ?>
+    <a class="o-widget__link o-widget__link--related" href="#">
+      <?php echo $link; ?>
+    </a>
+  <?php
+    endforeach;
+  ?>
+</section>
--- partials/widget/links-rich.php
+++ partials/widget/links-rich.php
@@ -1,3 +1,28 @@
 <?php
 // Sidebar widget to display image-enhanced links with descriptions
 ?>
+<section class="o-widget">
+  <?php
+    // Placeholder content to mimic what feeds from ACF
+    $links = array(
+      'Locate a Distributor',
+      'Request a Quote',
+      'Request Support',
+    );
+    foreach( $links as $link ):
+  ?>
+    <a class="o-widget__link" href="#">
+      <div class="m-richLink">
+        <img class="m-richLink__image lazyload" src="<?php placeholder_img(145,145,'text=icon'); ?>" />
+        <div class="m-richLink__content">
+          <?php echo $link; ?><br />
+          <span class="m-richLink__content--description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </span>
+        </div>
+      </div>
+    </a>
+  <?php
+    endforeach;
+  ?>
+</section>
