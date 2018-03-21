d2d2f39b4fb39d06659dfcb32283309cf4e1aba0
Ben Davidson <ben@bendavidson.me>
2018-03-21 12:02:18 -0400

    Updated layout and styles for general block module

--- partials/content/general-block.php
+++ partials/content/general-block.php
@@ -1,3 +1,32 @@
 <?php
 // General purpose content block
 ?>
+<section class="o-generalBlock">
+  <div class="o-generalBlock__content">
+    <h2>
+      Lorem Ipsum Dolor Sit amet, Consect Etur Adipiscing
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam si varietatem diceres, intellegerem, ut etiam non dicente te intellego; Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Quamquam te quidem video minime esse deterritum. Idem iste, inquam, de voluptate quid sentit? Omnia peccata paria dicitis.
+    </p>
+    <h3>
+      Quid, quod homines infima fortuna
+    </h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam si varietatem diceres, intellegerem, ut etiam non dicente te intellego; Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Quamquam te quidem video minime esse deterritum. Idem iste, inquam, de voluptate quid sentit? Omnia peccata paria dicitis.
+    </p>
+    <img src="<?php placeholder_img(725,250,'text=image'); ?>" />
+    <h3>
+      Quid, quod homines infima fortuna
+    </h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam si varietatem diceres, intellegerem, ut etiam non dicente te intellego; Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Quamquam te quidem video minime esse deterritum. Idem iste, inquam, de voluptate quid sentit? Omnia peccata paria dicitis.
+    </p>
+    <h2>
+      Lorem Ipsum Dolor Sit amet, Consect Etur Adipiscing
+    </h2>
+    <p>
+      Cupiditates non Epicuri divisione finiebat, sed sua satietate. Ad quorum et cognitionem et usum iam corroborati natura ipsa praeeunte deducimur.
+    </p>
+  </div>
+</section>
