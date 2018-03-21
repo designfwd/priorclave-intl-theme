379ae4dfca97febf91ccc6ffa979c383f4f8cb08
Ben Davidson <ben@bendavidson.me>
2018-03-21 12:04:08 -0400

    Updated markup for social sharing widget

--- partials/widget/social-sharing.php
+++ partials/widget/social-sharing.php
@@ -1,3 +1,12 @@
 <?php
 // Social media sharing links, arranged in icon blocks
 ?>
+<section class="o-sidebarSocial">
+  <?php // Social Media Feather sharing icons
+    if(
+      function_exists(‘synved_social_share_markup’)
+      ):
+      echo synved_social_share_markup();
+    endif;
+  ?>
+</section>
