8a3a3e92688f00f3cf46f43e660444bfd970c75b
Ben Davidson <ben@bendavidson.me>
2018-03-21 14:28:23 -0400

    Added form layout

--- partials/widget/newsletter-signup.php
+++ partials/widget/newsletter-signup.php
@@ -1,3 +1,24 @@
 <?php
 // Sidebar widget to display a newsletter signup form
 ?>
+<section class="o-widget">
+  <h2 class="o-widget__headline">Stay Informed</h2>
+  <div class="o-widget__form">
+
+    <form class="m-widgetForm">
+      <div class="gform_body">
+        <ul class="gform_fields">
+          <li class="gfield m-widgetForm__email">
+            <div class="ginput_container">
+              <input type="text" class="large" placeholder="Email Address" />
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="gform_footer ">
+        <input type="submit" class="gform_button button" value="Subscribe" />
+      </div>
+    </form>
+
+  </div>
+</section>
