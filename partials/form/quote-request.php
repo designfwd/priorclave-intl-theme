fcc1f05a6187897b9875732775f48103469e0f15
Ben Davidson <ben@bendavidson.me>
2018-03-21 16:16:28 -0400

    Updated markup and styles for quote request module

--- partials/form/quote-request.php
+++ partials/form/quote-request.php
@@ -1,3 +1,20 @@
 <?php
   // A content block with form for submitting quote requests
 ?>
+<section class="o-quoteRequest">
+  <h2 class="o-quoteRequest__headline">
+    Lorem Ipsum Dolor Sit Amet Consectetur
+  </h2>
+  <div class="o-quoteRequest__description">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam si varietatem diceres, intellegerem, ut etiam non dicente te intellego; Nam illud vehementer repugnat, eundem beatum esse et multis malis.
+    </p>
+  </div>
+  <div class="o-quoteRequest__form">
+
+  </div>
+  <div class="o-quoteRequest__subtitle">
+    Or Contact an Expert by Phone at:<br />
+  </div>
+  <a class="o-quoteRequest__phone" href="tel:+12 34 5678 9012">+12 34 5678 9012</a>
+</section>
