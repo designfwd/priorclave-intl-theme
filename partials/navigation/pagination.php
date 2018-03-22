bcdbb23ce418e09666361743725ed1747b580176
Ben Davidson <ben@bendavidson.me>
2018-03-22 16:39:46 -0400

    Updated styles for pagination

--- partials/navigation/pagination.php
+++ partials/navigation/pagination.php
@@ -1,3 +1,28 @@
 <?php
 // Pagination links for placement on archive pages
 ?>
+<section class="o-pagination">
+  <div class="o-pagination__adjacent o-pagination__adjacent--previous" data-href="#">
+    Previous
+  </div>
+  <div class="o-pagination__pages">
+    <?php
+      $count = 10;
+      for( $i=0; $i<$count; $i++ ):
+    ?>
+      <a class="o-pagination__pages--link" href="#"><?php echo $i+1; ?></a>
+      <?php
+        if( $i<($count-1) ):
+      ?>
+        <span class="o-pagination__pages--divider"> - </span>
+      <?php
+        endif;
+      ?>
+    <?php
+      endfor;
+    ?>
+  </div>
+  <div class="o-pagination__adjacent o-pagination__adjacent--next" data-href="#">
+    Next
+  </div>
+</section>
