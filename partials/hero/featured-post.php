b9598d44f743df6a38e57177a0554d56e02d429e
Ben Davidson <ben@bendavidson.me>
2018-03-22 12:38:09 -0400

    Updated markup and styles for Hero Featured Post module

--- partials/hero/featured-post.php
+++ partials/hero/featured-post.php
@@ -1,3 +1,17 @@
 <?php
 // Preview of a specific post for feature
 ?>
+<section class="o-heroPost">
+  <a href="#">
+    <img class="o-heroPost__image lazyload" src="<?php placeholder_img(240,240,'text=image'); ?>" />
+  </a>
+  <div class="o-heroPost__content">
+    <a class="o-heroPost__content--headline" href="#">Priorclave at Medlab 2018</a>
+    <br />
+    <span class="o-heroPost__content--description">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quantum Aristoxeni ingenium consumptum videmus in musicis.  Lorem ipsum dolor sit amet...
+    </span>
+    <br />
+    <a class="o-heroPost__content--link" href="#">Read More</a>
+  </div>
+</section>
