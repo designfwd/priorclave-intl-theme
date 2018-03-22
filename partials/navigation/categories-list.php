9c70e8e37dad020b31414b1b7022a0c66b3652d7
Ben Davidson <ben@bendavidson.me>
2018-03-22 16:42:38 -0400

    Added categories list markup

--- partials/navigation/categories-list.php
+++ partials/navigation/categories-list.php
@@ -1,3 +1,39 @@
 <?php
 // A list of links displayed as a comma-separated, continual expression
 ?>
+<section class="o-categoriesList">
+  <div class="o-categoriesList__headline">
+    Categories:
+  </div>
+  <div class="o-categoriesList__categories">
+    <?php
+      // List of categories as placeholders
+      $categories = array(
+        'Product Updates',
+        'Company News',
+        'Service & Support',
+        'Events',
+        'Product Updates',
+        'Company News',
+        'Service & Support',
+        'Events'
+      );
+
+      for( $i=0; $i<count($categories); $i++ ):
+    ?>
+      <?php
+        if($i<(count($categories)-1)):
+      ?>
+        <a class="o-categoriesList__categories--link" href="#"><?php echo $categories[$i]; ?>, </a>
+      <?php
+        else:
+      ?>
+        <a class="o-categoriesList__categories--link" href="#"><?php echo $categories[$i]; ?></a>
+      <?php
+        endif;
+      ?>
+    <?php
+      endfor;
+    ?>
+  </div>
+</section>
