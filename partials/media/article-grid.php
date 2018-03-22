5f94b06b840bc5efa259cb17312a9fb40d102caa
Ben Davidson <ben@bendavidson.me>
2018-03-22 14:38:43 -0400

    Updated article grid styles

--- partials/media/article-grid.php
+++ partials/media/article-grid.php
@@ -1,3 +1,22 @@
 <?php
-  // Grid of blog articles
+  // Grid of blog article previews
 ?>
+<section class="o-articleGrid">
+  <?php
+    for( $i=0; $i<rand(7,11); $i++):
+  ?>
+    <div class="o-articleGrid__article">
+      <div class="m-articlePreview m-articlePreview--blog" data-href="#">
+          <img class="m-articlePreview__featuredImage lazyload" src="<?php get_picsum(648,382); ?>" />
+          <img class="m-articlePreview__photo lazyload" src="<?php get_picsum(72,72); ?>" />
+          <h3 class="m-articlePreview__headline" href="#">Lorem Ipsum Dolor Sit</h3>
+          <div class="m-articlePreview__article">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation...
+          </div>
+          <a class="m-articlePreview__link" href="#">Read More</a>
+      </div>
+    </div>
+  <?php
+    endfor;
+  ?>
+</section>
--- partials/media/featured-posts.php
+++ partials/media/featured-posts.php
@@ -6,7 +6,17 @@
   <div class="o-featuredPosts__articles">
     <?php
       for( $i=0; $i<3; $i++):
-        get_partial('content/article-preview');
+    ?>
+      <div class="m-articlePreview" data-href="#">
+          <img class="m-articlePreview__featuredImage lazyload" src="<?php get_picsum(648,382); ?>" />
+          <img class="m-articlePreview__photo lazyload" src="<?php get_picsum(72,72); ?>" />
+          <h3 class="m-articlePreview__headline" href="#">Lorem Ipsum Dolor Sit</h3>
+          <div class="m-articlePreview__article">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation...
+          </div>
+          <a class="m-articlePreview__link" href="#">Read More</a>
+      </div>
+    <?php
       endfor;
     ?>
   </div>
