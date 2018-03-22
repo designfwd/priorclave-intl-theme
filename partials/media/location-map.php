937a326f6f1d6a5ecb711080541c25a4aa9fafcb
Ben Davidson <ben@bendavidson.me>
2018-03-22 12:18:46 -0400

    Added placeholder markup

--- partials/media/location-map.php
+++ partials/media/location-map.php
@@ -1,3 +1,8 @@
 <?php
 // Static Google map with specific location information
 ?>
+<section class="o-locationMap">
+  <div class="o-locationMap__map">
+    <?php // This will be populated with shortcode content that is difficult to replicate without ACF and plugin already set up. ?>
+  </div>
+</section>
