3d0230b0b1d65a60b4df6413eac7b9abcc2db872
Ben Davidson <ben@bendavidson.me>
2018-03-23 13:54:56 -0400

    Added markup and styles for search form

--- partials/form/site-search.php
+++ partials/form/site-search.php
@@ -1,3 +1,14 @@
 <?php
 // Search form for use within the page body
 ?>
+<section class="o-siteSearch">
+  <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
+  <form role="search" method="get" class="search-form o-siteSearch__form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="Search" value="<?php echo get_search_query(); ?>" name="s" />
+  	<button type="submit" class="search-submit">
+      <svg viewBox="0 0 16 16">
+        <?php get_svg( 'icon-search' ); ?>
+      </svg>
+      <span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'bellhop' ); ?></span></button>
+  </form>
+</section>
