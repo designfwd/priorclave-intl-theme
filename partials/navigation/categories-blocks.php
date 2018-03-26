aedda76135d17cf5be985e16b6e2d0d7c725322d
Ben Davidson <ben@bendavidson.me>
2018-03-26 14:59:53 -0400

    Updated  markup and styles for category navigation block module

--- partials/navigation/categories-blocks.php
+++ partials/navigation/categories-blocks.php
@@ -1,3 +1,27 @@
 <?php
 // A list of category links arranged in blocks
 ?>
+<section class="o-categoryNav">
+  <?php
+    // Options to be passed via ACF
+    $category = 'region'; // Used as a modifier for IDs
+    $options = array(
+      'Africa',
+      'Asia Pacific',
+      'Europe',
+      'Middle East',
+      'North America',
+      'South America',
+    );
+
+    foreach( $options as $option ):
+  ?>
+    <a class="o-categoryNav__link" href="#<?php echo $category . '-' . urlencode($option); ?>">
+      <div class="o-categoryNav__block">
+        <?php echo $option; ?>
+      </div>
+    </a>
+  <?php
+    endforeach;
+  ?>
+</section>
