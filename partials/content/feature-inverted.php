1c12d916cde587a2ac538bdb17b2a6c1f97d9e15
Ben Davidson <ben@bendavidson.me>
2018-03-27 13:31:12 -0400

    Updated markup and styles

--- partials/content/feature-inverted.php
+++ partials/content/feature-inverted.php
@@ -1,3 +1,11 @@
 <?php
 // A block of content featured after hero and introductory content, with inverted colors
 ?>
+<section class="o-featureBlock o-featureBlock--inverted">
+  <h2 class="o-featureBlock__headline o-featureBlock__headline--inverted">Other Resources</h2>
+  <div class="o-featureBlock__content">
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+    </p>
+  </div>
+</section>
