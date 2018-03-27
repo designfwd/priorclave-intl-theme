8cf373924296246f8a01c2e8c25e55429d59aa1a
Ben Davidson <ben@bendavidson.me>
2018-03-27 15:06:32 -0400

    Added video grid layout and styles

--- partials/media/video-grid.php
+++ partials/media/video-grid.php
@@ -1,3 +1,14 @@
 <?php
 // Grid of video previews
 ?>
+<section class="o-videoGrid">
+  <?php
+    for( $i=0; $i<9; $i++ ):
+  ?>
+    <div class="o-videoGrid__video">
+      <iframe src='https://www.youtube.com/embed/QILiHiTD3uc' frameborder='0' allowfullscreen></iframe>
+    </div>
+  <?php
+    endfor;
+  ?>
+</section>
