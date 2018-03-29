9c6435594a2e27710e2ea9fcb1cdd65965639c0a
Ben Davidson <ben@bendavidson.me>
2018-03-29 14:04:20 -0400

    Updated markup to allow for preset variables

--- partials/navigation/simplified.php
+++ partials/navigation/simplified.php
@@ -1,9 +1,41 @@
 <?php
-// Navigation to use in lieu of primary nav on international homepages
+/**
+ * Navigation to use in lieu of primary nav on international homepages
+ *
+ * Displays a non-clickable logo and a contact button. The contact button links to the contact page as
+ * as identified in the child site settings.
+ *
+ * @var array $logo   An image array set via ACF that shows the website logo
+ * @var string $url   A string denoting the URL of the website contact page
+ */
+
+// Assigns defaults if the variables are not set
+if( !isset($logo) && $logo == '' ):
+  $logo = array(
+    'alt' => 'logo',
+    'sizes' => array(
+      'preload' => '//via.placeholder.com/64x26?text=logo',
+      '128w' => '//via.placeholder.com/128x51?text=logo',
+    ),
+    'url' => '//via.placeholder.com/192x76?text=logo'
+  );
+endif;
+
+if( !isset($url) && $url == '' ):
+  $url = '#';
+endif;
 ?>
 <section class="o-simpleNav">
-  <img class="o-simpleNav__logo lazyload lazyload--blurUp" src="<?php placeholder_img( 192, 76, 'text=logo'); ?>" />
+  <img class="o-simpleNav__logo lazyload lazyload--blurUp"
+    alt="<?php echo $logo['alt']; ?>"
+    src="<?php echo $logo['sizes']['preload']; ?>"
+    data-sizes="auto"
+    data-srcset="<?php echo $logo['sizes']['preload']; ?> 16w,
+      <?php echo $logo['sizes']['128w']; ?> 65w,
+      <?php echo $logo['url']; ?> 129w
+    "
+  />
   <div class="o-simpleNav__button">
-    <a class="a-navButton" href="#">Contact</a>
+    <a class="a-navButton" href="<?php echo $url; ?>">Contact</a>
   </div>
 </section>
