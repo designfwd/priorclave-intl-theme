d1c6f41d5eca14164425b371f17228c2162cf3d5
Ben Davidson <ben@bendavidson.me>
2018-04-04 12:15:54 -0400

    Added ACF compatibility

--- partials/footer/quicklinks.php
+++ partials/footer/quicklinks.php
@@ -1,14 +1,60 @@
 <?php
-// A group of button-like links
+/**
+ * Footer quicklinks
+ *
+ * A group of button-like links in the universal footer area
+ *
+ * @var array $links    Links to use in the footer
+ */
+
+// Sets variables, with fallbacks if ACF is not installed or if variables are empty
+if( function_exists('get_field') ):
+  $links = 'footer_quicklinks';
+endif;
+
+if(
+  !function_exists('get_field') ||
+  (function_exists('get_field') && !have_rows($links))
+):
+  $linksFallback = true;
+  $links = array(
+    array(
+      'label' => 'Find a Distributor',
+      'url' => '#',
+    ),
+    array(
+      'label' => 'Request a Quote',
+      'url' => '#',
+    ),
+    array(
+      'label' => 'Request Support',
+      'url' => '#'
+    )
+  );
+endif;
 ?>
 <section class="o-footerQuicklinks">
-  <a class="o-footerQuicklinks__button" href="#">
-    Find a Distributor
-  </a>
-  <a class="o-footerQuicklinks__button" href="#">
-    Request a Quote
-  </a>
-  <a class="o-footerQuicklinks__button" href="#">
-    Request Support
-  </a>
+  <?php
+    if( $linksFallback != true ):
+      while( have_rows($links, 'option') ):
+        $label = get_sub_field('label');
+        $url = get_sub_field('url');
+    ?>
+      <a class="o-footerQuicklinks__button" href="<?php echo $url; ?>">
+        <?php echo $label; ?>
+      </a>
+    <?php
+      endwhile;
+    else:
+      foreach( $links as $link ):
+        $label = $link['label'];
+        $url = $link['url'];
+    ?>
+      <a class="o-footerQuicklinks__button" href="<?php echo $url; ?>">
+        <?php echo $label; ?>
+      </a>
+    <?php
+      endforeach;
+    endif;
+  ?>
 </section>
