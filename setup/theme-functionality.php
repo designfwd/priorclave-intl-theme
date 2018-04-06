d8d5f9e23d745466d9b2624e1246c67f3a1df55f
Ben Davidson <ben@bendavidson.me>
2018-04-06 13:17:45 -0400

    Formatting updates

--- setup/theme-functionality.php
+++ setup/theme-functionality.php
@@ -1,7 +1,6 @@
 <?php
 function bellhop_supports() {
   // Make theme available for translation
-  // Community translations can be found at https://github.com/roots/sage-translations
   load_theme_textdomain('bellhop', get_template_directory() . '/lang');
 
   // Enable plugins to manage the document title
@@ -18,34 +17,38 @@ function bellhop_supports() {
 }
 add_action( 'after_setup_theme', 'bellhop_supports' );
 
+
 // Removes unnecessary items from the admin menu
 function bellhop_remove_menus() {
   // remove_menu_page('edit.php');
-  // remove_menu_page('edit-comments.php');
+  remove_menu_page('edit-comments.php');
   remove_menu_page('link-manager.php');
 }
 add_action( 'admin_menu', 'bellhop_remove_menus');
+
 
 // Renames the default page template to encourage use of custom templates
 add_filter('default_page_template_title', function() {
   return __('--Pick a page template--', 'bellhop');
 });
 
+
 // Removes the admin bar for all but admins on the front-end
 if (!current_user_can('administrator')) :
   // show_admin_bar(false);
 endif;
 
+
 // Updates initial ACF settings
 function bellhop_acf_init() {
-
   acf_update_setting('save_json', (get_stylesheet_directory() . '/assets/json/acf/'));
   acf_update_setting('load_json', (get_stylesheet_directory() . '/assets/json/acf/'));
 }
 add_action('acf/init', 'bellhop_acf_init');
 
+
 // Creates ACF Options page
-if( function_exists('acf_add_options_page') ) {
+if( function_exists('acf_add_options_page') ):
 
   acf_add_options_page(array(
     'page_title'  => 'Theme Settings',
@@ -57,7 +60,8 @@ if( function_exists('acf_add_options_page') ) {
     'icon_url'    => 'dashicons-index-card'
   ));
 
-  if( is_multisite() && (get_current_blog_id() == 1) ){
+  // Parent site settings
+  if( is_multisite() && (get_current_blog_id() == 1) ):
     acf_add_options_page(array(
       'page_title'  => 'Parent Site Settings',
       'menu_title'  => 'Parent Site Settings',
@@ -67,18 +71,20 @@ if( function_exists('acf_add_options_page') ) {
       'position'    => '58',
       'icon_url'    => 'dashicons-forms'
     ));
-  }
+  endif;
+endif;
 
-}
 
 // Simplified partials integration for cleaner markup
 function get_partial( $slug ) {
   return include(locate_template('partials/' . $slug . '.php'));
 }
 
+
 function get_svg( $file ) {
   echo file_get_contents( get_stylesheet_directory_uri() . '/assets/images/dist/svg/' . $file . '.svg');
 }
+
 
 function placeholder_img( $width, $height, $text='' ) {
   if( $text != ''):
@@ -88,6 +94,7 @@ function placeholder_img( $width, $height, $text='' ) {
     echo ( '//via.placeholder.com/' . $width . 'x' . $height );
   endif;
 }
+
 
 function get_picsum( $width, $height='', $modifier='' ) {
   // If a height is defined, append a '/' to it for the final URL
