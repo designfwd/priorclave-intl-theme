99434ae14be6fba1f3c43c3f2aed7e1357195a98
Ben Davidson <ben@bendavidson.me>
2018-04-06 16:54:34 -0400

    Removed international menu - it will be created via ACF

--- setup/menus.php
+++ setup/menus.php
@@ -6,14 +6,5 @@ function bellhop_menus() {
       'primary_nav' => __('Primary Navigation', 'bellhop')
     ]
   );
-
-  // If the website is multisite and the current site is the supersite, show the region menu
-  if( is_multisite() && (get_current_blog_id() == 1) ):
-    register_nav_menus(
-      [
-        'international_nav' => __('International Navigation', 'bellhop')
-      ]
-    );
-  endif;
 }
 add_action( 'after_setup_theme', 'bellhop_menus');
