6b36ee8e7ad44944e45d4a3dbbf0df8777edd5f4
Ben Davidson <ben@bendavidson.me>
2018-04-06 12:32:25 -0400

    Updated formatting

--- setup/theme-functionality.php
+++ setup/theme-functionality.php
@@ -1,22 +1,20 @@
 <?php
-
 function bellhop_supports() {
-// Make theme available for translation
-// Community translations can be found at https://github.com/roots/sage-translations
-load_theme_textdomain('bellhop', get_template_directory() . '/lang');
+  // Make theme available for translation
+  // Community translations can be found at https://github.com/roots/sage-translations
+  load_theme_textdomain('bellhop', get_template_directory() . '/lang');
 
-// Enable plugins to manage the document title
-// http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
-add_theme_support('title-tag');
+  // Enable plugins to manage the document title
+  // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
+  add_theme_support('title-tag');
 
-// Enable post formats
-// http://codex.wordpress.org/Post_Formats
-add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
+  // Enable post formats
+  // http://codex.wordpress.org/Post_Formats
+  add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
 
-// Enable HTML5 markup support
-// http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
-add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
-
+  // Enable HTML5 markup support
+  // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
+  add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
 }
 add_action( 'after_setup_theme', 'bellhop_supports' );
 
