22fd40c415592dd440f97277184a1c76184726dc
Ben Davidson <ben@bendavidson.me>
2018-04-09 11:38:14 -0400

    Implemented ACF and replaced WP menu styles

--- partials/navigation/regional.php
+++ partials/navigation/regional.php
@@ -12,147 +12,47 @@
 // Sets variables with fallbacks if they're unset or if ACF is not in use
 if( function_exists('get_field') ):
   $ID = get_the_ID();
-  $options = get_field('hero_mapHomepage_options', $ID);
+  $options = 'hero_mapHomepage_options';
   $language = get_field('site_language', 'option')['value'];
 endif;
-
-if( !isset($headline) || ($headline == '') ):
-  $headline = 'Reliable. Flexible. Programmable. Affordable';
-endif;
-
-if( !isset($subhead) || ($subhead == '') ):
-  $subhead = 'Autoclaves Worldwide';
-endif;
-
-if( !isset($options) || ($options == '') ):
-  $options = array(
-    array(
-      'slug' => 'africa',
-      'name' => 'Africa',
-    ),
-    array(
-      'slug' => 'asia-pacific',
-      'name' => 'Asia Pacific',
-      'countries' => array(
-        array(
-          'abbreviation' => 'au',
-          'name' => 'Australia',
-          'external_site' => false,
-        ),
-        array(
-          'abbreviation' => 'my',
-          'name' => 'Malaysia',
-          'external_site' => false,
-        ),
-        array(
-          'abbreviation' => 'ph',
-          'name' => 'Philippines',
-          'external_site' => false,
-        ),
-      ),
-    ),
-    array(
-      'slug' => 'europe',
-      'name' => 'Europe',
-      'countries' => array(
-        array(
-          'abbreviation' => 'gb',
-          'name' => 'United Kingdom',
-          'external_site' => true
-        ),
-      ),
-    ),
-    array(
-      'slug' => 'middle-east',
-      'name' => 'Middle East',
-    ),
-    array(
-      'slug' => 'north-america',
-      'name' => 'North America',
-      'countries' => array(
-        array(
-          'abbreviation' => 'ca',
-          'name' => 'Canada',
-          'external_site' => false,
-        ),
-        array(
-          'abbreviation' => 'us',
-          'name' => 'United States',
-          'external_site' => true,
-        ),
-      ),
-    ),
-    array(
-      'slug' => 'south-america',
-      'name' => 'South America'
-    )
-  );
-endif;
-
 if( !isset($language) || ($language == '') ):
   $language = 'en';
 endif;
 ?>
 <nav class="o-regionalNav">
-
-  <?php
-    // Rough multiregion navigation structure, mimicing WP div structure
-    if( has_nav_menu('international_nav') ):
-      $args = array(
-        'menu' => 'international_nav',
-        'menu_class' => 'o-regionalNav__menu',
-        'fallback_cb' => false,
-      );
-      wp_nav_menu($args);
-    else:
-  ?>
-    <div>
-      <ul class="o-regionalNav__menu">
-        <li class="menu-item-depth-0 menu-item">
-          <a class="menu-link" href="#">Africa</a>
-        </li>
-        <li class="menu-item-depth-0 menu-item menu-item-has-children">
-          <a class="menu-link" href="#">Asia Pacific</a>
-          <ul class="sub-menu menu-depth-1">
-            <li class="sub-menu-item menu-item-depth-1 menu-item">
-              <a class="menu-link sub-menu-link" href="#">Australia</a>
-            </li>
-            <li class="sub-menu-item menu-item-depth-1 menu-item">
-              <a class="menu-link sub-menu-link" href="#">Malaysia</a>
-            </li>
-            <li class="sub-menu-item menu-item-depth-1 menu-item">
-              <a class="menu-link sub-menu-link" href="#">Philippines</a>
-            </li>
-          </ul>
-        </li>
-        <li class="menu-item-depth-0 menu-item menu-item-has-children">
-          <a class="menu-link" href="#">Europe</a>
-          <ul class="sub-menu menu-depth-1">
-            <li class="sub-menu-item menu-item-depth-1 menu-item">
-              <a class="menu-link sub-menu-link" href="#">United Kingdom</a>
-            </li>
-          </ul>
-        </li>
-        <li class="menu-item-depth-0 menu-item">
-          <a class="menu-link" href="#">Middle East</a>
-        </li>
-        <li class="menu-item-depth-0 menu-item menu-item-has-children">
-          <a class="menu-link" href="#">North America</a>
-          <ul class="sub-menu menu-depth-1">
-            <li class="sub-menu-item menu-item-depth-1 menu-item">
-              <a class="menu-link sub-menu-link" href="#">Canada</a>
-            </li>
-            <li class="sub-menu-item menu-item-depth-1 menu-item">
-              <a class="menu-link sub-menu-link" href="#">United States</a>
-            </li>
-          </ul>
-        </li>
-        <li class="menu-item-depth-0 menu-item">
-          <a class="menu-link" href="#">South America</a>
-        </li>
-      </ul>
-    </div>
-  <?php
-    endif;
-  ?>
+  <ul class="o-regionalNav__menu">
+    <?php
+      if( have_rows($options, $ID) ):
+        while( have_rows( $options, $ID) ): the_row();
+          $region = get_sub_field('region');
+          $regionLink = get_site_url(1) . '/' . $language . '/' . $region['value'];
+        ?>
+          <li class="o-regionalNav__region">
+            <a class="o-regionalNav__region--link" href="<?php echo $regionLink; ?>">
+              <?php echo $region['label']; ?>
+            </a>
+          </li>
+          <?php
+            if( have_rows('countries') ):
+              while( have_rows('countries') ): the_row();
+                $country = get_sub_field('country');
+                $separateCheck = get_sub_field('separate');
+                if( $separateCheck == true ):
+                  $countryLink = get_site_url(1) . '/' . $language . '-' . $country['value'] . '/';
+                else:
+                  $countryLink = get_site_url(1) . '/' . $language . '/' . str_replace(' ', '-', strtolower($country['label']));
+                endif;
+              ?>
+                <li class="o-regionalNav__country">
+                  <a class="o-regionalNav__country--link" href="<?php echo $countryLink; ?>">
+                    <?php echo $country['label']; ?>
+                  </a>
+                </li>
+              <?php
+              endwhile;
+            endif;
+        endwhile;
+      endif;
+    ?>
+  </ul>
 </nav>
