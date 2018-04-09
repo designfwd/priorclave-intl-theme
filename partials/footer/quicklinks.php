81a840a16f77c88b268e97a37fe679548b8a89f6
Ben Davidson <ben@bendavidson.me>
2018-04-09 15:59:52 -0400

    Added Quicklinks ACFs

--- partials/footer/quicklinks.php
+++ partials/footer/quicklinks.php
@@ -11,50 +11,24 @@
 if( function_exists('get_field') ):
   $links = 'footer_quicklinks';
 endif;
-
-if(
-  !function_exists('get_field') ||
-  (function_exists('get_field') && !have_rows($links))
-):
-  $linksFallback = true;
-  $links = array(
-    array(
-      'label' => 'Find a Distributor',
-      'url' => '#',
-    ),
-    array(
-      'label' => 'Request a Quote',
-      'url' => '#',
-    ),
-    array(
-      'label' => 'Request Support',
-      'url' => '#'
-    )
-  );
-endif;
 ?>
 <section class="o-footerQuicklinks">
   <?php
-    if( $linksFallback != true ):
+    if( have_rows($links, 'option') ):
       while( have_rows($links, 'option') ): the_row();
         $label = get_sub_field('label');
-        $url = get_sub_field('url');
+        $linkType = get_sub_field('linkType');
+        if( $linkType == true ):
+          $url = get_sub_field('page');
+        else:
+          $url = get_sub_field('url');
+        endif;
     ?>
       <a class="o-footerQuicklinks__button" href="<?php echo $url; ?>">
         <?php echo $label; ?>
       </a>
     <?php
       endwhile;
-    else:
-      foreach( $links as $link ):
-        $label = $link['label'];
-        $url = $link['url'];
-    ?>
-      <a class="o-footerQuicklinks__button" href="<?php echo $url; ?>">
-        <?php echo $label; ?>
-      </a>
-    <?php
-      endforeach;
     endif;
   ?>
 </section>
