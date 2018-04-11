899e381604d171c0dc866ce21d3d928312cbfd7e
Ben Davidson <ben@bendavidson.me>
2018-04-11 13:32:00 -0400

    Added Simple Call-to-Action ACF integration

--- partials/media/cta-simple.php
+++ partials/media/cta-simple.php
@@ -1,12 +1,27 @@
 <?php
-// Simple call-to-action with a headline, body, and page-linked button
+/**
+ * Simple Call to Action
+ *
+ * Simple call-to-action with a headline, body, and page-linked button
+ *
+ * @var string $headline      Headline text for this area
+ * @var string $description   Descriptive text to include for this CTA
+ * @var string $label         Label text for the button
+ * @var string $url           URL of linked page for button
+ */
+
+if( function_exists('get_field') ):
+  $ID = get_the_ID();
+  $headline = get_field('media_ctaSimple_headline', $ID);
+  $description = get_field('media_ctaSimple_description', $ID);
+  $label = get_field('media_ctaSimple_label', $ID);
+  $url = get_field('media_ctaSimple_url', $ID);
+endif;
 ?>
 <section class="o-ctaSimple">
-  <h2 class="o-ctaSimple__headline">Get Pricing for Your Perfect Autoclave Fast</h2>
+  <h2 class="o-ctaSimple__headline"><?php echo $headline; ?></h2>
   <div class="o-ctaSimple__description">
-    <p>
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
-    </p>
+    <?php echo $description; ?>
   </div>
-  <a class="o-ctaSimple__button" href="#">Request a Quote</a>
+  <a class="o-ctaSimple__button" href="<?php echo $url; ?>"><?php echo $label; ?></a>
 </section>
