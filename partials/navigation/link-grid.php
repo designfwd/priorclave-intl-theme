fc8bf01e3e96fbd906a377cfc3aed0ab3ce8fc03
Ben Davidson <ben@bendavidson.me>
2018-04-11 15:30:43 -0400

    Added Link Grid navigation ACFs

--- partials/navigation/link-grid.php
+++ partials/navigation/link-grid.php
@@ -1,29 +1,46 @@
 <?php
-// Grid of linked pages with background imagery and rollover interaction
+/**
+ * Link grid
+ *
+ * Grid of linked pages with background imagery and rollover interaction
+ *
+ * @var string $links     ACF field to use for links array
+ * @var string $label     The label to give to the link
+ * @var object $page      Linked post object
+ */
+
+if( function_exists('get_field') ):
+  $ID = get_the_ID();
+  $links = 'navigation_linkGrid_links';
+endif;
 ?>
 <section class="o-linkGrid">
   <?php
-    $grid = array(
-      'Education & Academia',
-      'Food & Beverage',
-      'Health & Personal Care',
-      'Test Laboratories',
-      'Life Science & Pharmaceutical',
-      'Veterinary Hospitals',
-      'Agriculture & Environment Research',
-      'Government Agencies',
-      'Manufacturing & Industry'
-    );
-
-    foreach ($grid as $item):
-  ?>
-  <a class="o-linkGrid__item" href="#" style="background-image:url('<?php get_picsum(854,591,'grayscale'); ?>')">
-    <div class="m-linkGridItem">
-      <h3 class="m-linkGridItem__title"><?php echo $item; ?></h3>
-      <div class="m-linkGridItem__overlay"></div>
-    </div>
-  </a>
-  <?php
-    endforeach;
+    if( have_rows($links, $ID) ):
+      while( have_rows($links, $ID) ): the_row();
+        $label = get_sub_field('label');
+        $page = get_sub_field('page');
+        setup_postdata( $page );
+        ?>
+        <?php
+          if( has_post_thumbnail() ):
+          ?>
+            <a class="o-linkGrid__item lazyload" href="<?php the_permalink(); ?>" style="background-image:url('<?php the_post_thumbnail('preload'); ?>')" data-bg="<?php the_post_thumbnail('960w'); ?>">
+          <?php
+          else:
+          ?>
+            <a class="o-linkGrid__item lazyload" href="<?php the_permalink(); ?>" style="background-image:url('//picsum.photos/g/64/64?blur')" data-bg="//picsum.photos/g/960/664">
+          <?php
+          endif;
+        ?>
+          <div class="m-linkGridItem">
+            <h3 class="m-linkGridItem__title"><?php echo $label; ?></h3>
+            <div class="m-linkGridItem__overlay"></div>
+          </div>
+        </a>
+        <?php
+        wp_reset_postdata();
+      endwhile;
+    endif;
   ?>
 </section>
