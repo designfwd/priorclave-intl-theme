0217e64ac179fb2b85bcba99b0f3c81a594b6e45
Ben Davidson <ben@bendavidson.me>
2018-04-12 15:48:13 -0400

    Added ACF integration to Featured Posts

--- partials/media/featured-posts.php
+++ partials/media/featured-posts.php
@@ -1,24 +1,66 @@
 <?php
-// A block of featured article previews
+/**
+ * Featured article previews
+ *
+ * Up to three featured articles shown as previews, with the other remaining
+ * spaces (if any) drawn from recent posts
+ *
+ * @var array $background       The background image for the section
+ * @var string $headline        Headline text for this section
+ * @var array $articles         The featured articles, if any
+ * @var array $exclude          The article IDs to exclude from WP_Query
+ */
+
+if( function_exists('get_field') ):
+  $ID = get_the_ID();
+  $background = get_field('media_featuredPosts_background', $ID);
+  $headline = get_field('media_featuredPosts_headline', $ID);
+  $articles = get_field('media_featuredPosts_articles', $ID);
+  $exclude = get_field('media_featuredPosts_articles', $ID, false);
+endif;
+
+if( !$background ):
+  $background = array(
+    'sizes' => array(
+      'preload' => '//picsum.photos/64/64/?blur',
+      '2560w' => '//picsum.photos/2560/1280',
+    ),
+  );
+endif;
 ?>
-<section class="o-featuredPosts" style="background-image:url('<?php get_picsum(2400,1200, 'grayscale'); ?>')">
-  <h2 class="o-featuredPosts__headline">News & Updates</h2>
+<section class="o-featuredPosts lazyload" data-bg="<?php echo $background['sizes']['2560w']; ?>" style="background-image:url('<?php $background['sizes']['preload']; ?>')">
+  <h2 class="o-featuredPosts__headline"><?php echo $headline; ?></h2>
   <div class="o-featuredPosts__articles">
     <?php
-      for( $i=0; $i<3; $i++):
-    ?>
-      <div class="m-articlePreview" data-href="#">
-          <img class="m-articlePreview__featuredImage lazyload" src="<?php get_picsum(648,382); ?>" />
-          <img class="m-articlePreview__photo lazyload" src="<?php get_picsum(72,72); ?>" />
-          <h3 class="m-articlePreview__headline" href="#">Lorem Ipsum Dolor Sit</h3>
-          <div class="m-articlePreview__article">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation...
-          </div>
-          <a class="m-articlePreview__link" href="#">Read More</a>
-      </div>
-    <?php
-      endfor;
+      if( $articles ):
+        $rows = count($articles);
+        foreach( $articles as $post ):
+          setup_postdata($post);
+          include( locate_template('partials/content/article-preview.php') );
+        endforeach;
+        wp_reset_postdata();
+      else:
+        $rows = 0;
+      endif;
+
+      if ($rows < 3):
+        $args = array(
+          'post_type' => 'post',
+          'post_status' => 'publish',
+          'posts_per_page' => (3-$rows),
+          'order' => 'DESC',
+          'orderby' => 'date',
+          'post__not_in' => $exclude,
+        );
+        $recent = new WP_Query( $args );
+        $recent_posts = $recent->get_posts();
+        foreach( $recent_posts as $post ):
+          setup_postdata($post);
+          include( locate_template('partials/content/article-preview.php') );
+        endforeach;
+        wp_reset_postdata();
+      endif;
     ?>
   </div>
-  <a class="o-featuredPosts__link" href="#">View All News</a>
+  <a class="o-featuredPosts__link" href="<?php echo get_post_type_archive_link('post'); ?>">View All News</a>
 </section>
