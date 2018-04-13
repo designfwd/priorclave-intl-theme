70de25695c1c525781b598c69eac9f1d810e377a
Ben Davidson <ben@bendavidson.me>
2018-04-13 15:10:26 -0400

    Added ACF integration to Intro Inverted block

--- partials/content/intro-inverted.php
+++ partials/content/intro-inverted.php
@@ -1,11 +1,19 @@
 <?php
-// Introductory content block with a headline and body content, with inverted colors from the primary
+/**
+ * Intro block
+ *
+ * Introductory content block with a headline and body content, with inverted
+ * colors from the primary
+ *
+ * @var string $headline    Header text for this section
+ * @var string $body        Text content for this section
+ */
 ?>
 <section class="o-introContent o-introContent--inverted">
-  <h1 class="o-introContent__headline">Leader in Steam Autoclaves</h1>
+  <h1 class="o-introContent__headline">
+    <?php echo $headline; ?>
+  </h1>
   <div class="o-introContent__content">
-    <p>
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-    </p>
+    <?php echo $body; ?>
   </div>
 </section>
