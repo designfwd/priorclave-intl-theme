4fe120cd5fc942f19e3328701cd5938c91192c26
Ben Davidson <ben@bendavidson.me>
2018-04-13 15:14:44 -0400

    Added ACF integration

--- partials/content/intro-inverted.php
+++ partials/content/intro-inverted.php
@@ -8,6 +8,12 @@
  * @var string $headline    Header text for this section
  * @var string $body        Text content for this section
  */
+
+if( function_exists('get_field') ):
+ $ID = get_the_ID();
+ $headline = get_field('content_introBlock_headline', $ID);
+ $body = get_field('content_introBlock_body', $ID);
+endif;
 ?>
 <section class="o-introContent o-introContent--inverted">
   <h1 class="o-introContent__headline">
