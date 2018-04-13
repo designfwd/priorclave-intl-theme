c26dc33db763068ccc545fce80c04976c28676c2
Ben Davidson <ben@bendavidson.me>
2018-04-13 16:46:51 -0400

    Added ACF integration

--- partials/media/product-related.php
+++ partials/media/product-related.php
@@ -1,24 +1,72 @@
 <?php
-// Content block to promote a particular product or cateogry, with linked button
+/**
+ * Related Info Block
+ *
+ * Content block to promote another page, with linked button
+ *
+ * @var string $headline      The header text for this section
+ * @var string $content       The content related to this section
+ * @var array $image          The image associated with this content
+ * @var string $label         Label for the section's button
+ * @var string $page          The URL of the linked page
+ */
+
+if( function_exists('get_field') ):
+  $ID = get_the_ID();
+  $headline = get_field('media_productRelated_headline', $ID);
+  $content = get_field('media_productRelated_content', $ID);
+  $image = get_field('media_productRelated_image', $ID);
+  $label = get_field('media_productRelated_label', $ID);
+  $page = get_field('media_productRelated_page', $ID);
+
+  // If variables are not set for this page, use site defaults
+  if( !$headline && !$content && !$image && !$page && !$label ):
+    $headline = get_field('default_related_headline', 'option');
+    $content = get_field('default_related_content', 'option');
+    $image = get_field('default_related_image', 'option');
+    $label = get_field('default_related_label', 'option');
+    $page = get_field('default_related_page', 'option');
+  endif;
+endif;
+
+// If $image is not set for the page or the site, use a fallback
+if( !$image ):
+  $image = array(
+    'alt' => '',
+    'sizes' => array(
+      'preload' => '//picsum.photos/64/96',
+      '128w' => '//picsum.photos/128/191',
+      '240w' => '//picsum.photos/240/358',
+      '320w' => '//picsum.photos/320/478',
+      '360w' => '//picsum.photos/360/537',
+    ),
+  );
+endif;
 ?>
 <section class="o-relatedProduct">
   <h2 class="o-relatedProduct__headline">
-    Priorclave Steam Autoclaves
+    <?php echo $headline; ?>
   </h2>
   <div class="o-relatedProduct__content">
     <div class="m-relatedContent">
       <div class="m-relatedContent__body">
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-        </p>
+        <?php echo $content; ?>
       </div>
       <div class="m-relatedContent__button">
-        <a class="a-bigButton" href="#">View Autoclaves</a>
+        <a class="a-bigButton" href="<?php echo $page; ?>"><?php echo $label; ?></a>
       </div>
     </div>
-    <img class="m-relatedContent m-relatedContent--image lazyload" src="<?php get_picsum(335,500); ?>" />
+    <img
+      class="m-relatedContent m-relatedContent--image lazyload lazyload--blurUp"
+      alt="<?php echo $image['alt']; ?>"
+      src="<?php echo $image['sizes']['preload']; ?>"
+      data-sizes="auto"
+      data-srcset="<?php echo $image['sizes']['preload']; ?> 64w,
+        <?php echo $image['sizes']['128w']; ?> 65w,
+        <?php echo $image['sizes']['240w']; ?> 129w,
+        <?php echo $image['sizes']['320w']; ?> 241w,
+        <?php echo $image['sizes']['360w']; ?> 321w,
+      "
+    />
   </div>
 </section>
