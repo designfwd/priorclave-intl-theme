684b0dd0fa616502e428490f6f14c9d46864600c
Ben Davidson <ben@bendavidson.me>
2018-04-13 16:46:42 -0400

    Added ACF integration

--- partials/content/general-block.php
+++ partials/content/general-block.php
@@ -1,32 +1,19 @@
 <?php
-// General purpose content block
+/**
+ * General content block
+ *
+ * General-purpose content block, containing only a single content area
+ *
+ * @var string $content       The content for this block
+ */
+
+if( function_exists('get_field') ):
+  $ID = get_the_ID();
+  $content = get_field('content_generalBlock_content', $ID);
+endif;
 ?>
 <section class="o-generalBlock">
   <div class="o-generalBlock__content">
-    <h2>
-      Lorem Ipsum Dolor Sit amet, Consect Etur Adipiscing
-    </h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam si varietatem diceres, intellegerem, ut etiam non dicente te intellego; Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Quamquam te quidem video minime esse deterritum. Idem iste, inquam, de voluptate quid sentit? Omnia peccata paria dicitis.
-    </p>
-    <h3>
-      Quid, quod homines infima fortuna
-    </h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam si varietatem diceres, intellegerem, ut etiam non dicente te intellego; Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Quamquam te quidem video minime esse deterritum. Idem iste, inquam, de voluptate quid sentit? Omnia peccata paria dicitis.
-    </p>
-    <img src="<?php placeholder_img(725,250,'text=image'); ?>" />
-    <h3>
-      Quid, quod homines infima fortuna
-    </h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eam si varietatem diceres, intellegerem, ut etiam non dicente te intellego; Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Quamquam te quidem video minime esse deterritum. Idem iste, inquam, de voluptate quid sentit? Omnia peccata paria dicitis.
-    </p>
-    <h2>
-      Lorem Ipsum Dolor Sit amet, Consect Etur Adipiscing
-    </h2>
-    <p>
-      Cupiditates non Epicuri divisione finiebat, sed sua satietate. Ad quorum et cognitionem et usum iam corroborati natura ipsa praeeunte deducimur.
-    </p>
+    <?php echo $content; ?>
   </div>
 </section>
