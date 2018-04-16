994058ab879aca29a2226406f8704c549891afc7
Ben Davidson <ben@bendavidson.me>
2018-04-16 15:05:45 -0400

    Added ACF integration to related links widget

--- partials/widget/links-related.php
+++ partials/widget/links-related.php
@@ -1,26 +1,37 @@
 <?php
-// Sidebar widget to display a list of page links
+/**
+ * Related links widget
+ *
+ * Sidebar widget to display a list of page links
+ *
+ * @var string $headline        Header text for this section
+ * @var string $links           The ACF field to check for links
+ * @var string $page            The linked page
+ * @var string $label           The label for the linked page
+ */
+
+if( function_exists('get_field') ):
+  $ID = get_the_ID();
+  $headline = get_field('widget_relatedLinks_headline', $ID);
+  $links = 'widget_relatedLinks_links';
+endif;
+
+if( have_rows($links, $ID) ):
 ?>
-<section class="o-widget">
-  <h2 class="o-widget__headline">Related Pages</h2>
-  <?php
-    // List of links as placeholder
-    $links = array(
-      'Excellent Customer Service',
-      'Unique Features',
-      'Quality, Reliability, & Safety',
-      'Energy & Water Efficiency',
-      'Accreditation & Certification',
-      'Customers & Case Studies',
-      'News',
-      'About Priorclave'
-    );
-    foreach( $links as $link ):
-  ?>
-    <a class="o-widget__link o-widget__link--related" href="#">
-      <?php echo $link; ?>
-    </a>
-  <?php
-    endforeach;
-  ?>
-</section>
+  <section class="o-widget">
+    <h2 class="o-widget__headline"><?php echo $headline; ?></h2>
+    <?php
+      while( have_rows($links, $ID) ): the_row();
+        $page = get_sub_field('page');
+        $label = get_sub_field('label');
+    ?>
+      <a class="o-widget__link o-widget__link--related" href="<?php echo $page; ?>">
+        <?php echo $label; ?>
+      </a>
+    <?php
+      endwhile;
+    ?>
+  </section>
+<?php
+endif;
+?>
