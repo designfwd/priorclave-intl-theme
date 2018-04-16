2bd92ac0075ce03e9411224980e619f60f95c97b
Ben Davidson <ben@bendavidson.me>
2018-04-16 16:48:00 -0400

    Added ACF integration for Location Map

--- partials/media/location-map.php
+++ partials/media/location-map.php
@@ -1,8 +1,23 @@
 <?php
-// Static Google map with specific location information
+/**
+ * Location map
+ *
+ * Static Google map with specific location information
+ *
+ * @var string $map       The map ID to pass to WP Google Maps
+ */
+
+if( function_exists('get_field') ):
+  $ID = get_the_ID();
+  $map = get_field('media_locationMap_map', $ID);
+endif;
 ?>
 <section class="o-locationMap">
   <div class="o-locationMap__map">
-    <?php // This will be populated with shortcode content that is difficult to replicate without ACF and plugin already set up. ?>
+    <?php
+      if( shortcode_exists('wpgmza') ):
+        echo do_shortcode("[wpgmza id=\"$map\"]");
+      endif;
+    ?>
   </div>
 </section>
