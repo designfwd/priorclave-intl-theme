51928c38031c684749b4e6c0512b41331c1c71ef
Ben Davidson <ben@bendavidson.me>
2018-04-16 12:19:37 -0400

    Added ACF integration for newsletter signup widget

--- partials/widget/newsletter-signup.php
+++ partials/widget/newsletter-signup.php
@@ -1,10 +1,24 @@
 <?php
-// Sidebar widget to display a newsletter signup form
+/**
+ * Newsletter widget
+ *
+ * Sidebar widget to display a newsletter signup form
+ *
+ * @var string $headline      Header text for this widget
+ * @var string $form          ID of the Gravity Forms object to import
+ */
+
+if( function_exists('get_field') ):
+  $headline = get_field('widget_newsletter_headline', 'option');
+  $form = get_field('widget_newsletter_form', 'option');
+endif;
 ?>
 <section class="o-widget">
-  <h2 class="o-widget__headline">Stay Informed</h2>
+  <h2 class="o-widget__headline"><?php echo $headline; ?></h2>
   <div class="o-widget__form">
-
+    <?php // If $form is defined, place Gravity Form here
+      if( !isset($form) || ($form == '') ):
+    ?>
     <form class="m-widgetForm">
       <div class="gform_body">
         <ul class="gform_fields">
@@ -19,6 +33,10 @@
         <input type="submit" class="gform_button button" value="Subscribe" />
       </div>
     </form>
-
+    <?php
+      elseif( isset($form) && function_exists('gravity_form') ):
+        gravity_form( $form, false, false, false, null, true, 1, true);
+      endif;
+    ?>
   </div>
 </section>
