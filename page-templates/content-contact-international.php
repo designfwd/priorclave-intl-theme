c0a7dcf8e63b04fa9175c0495cef935e29193afb
Ben Davidson <ben@bendavidson.me>
2018-04-17 11:52:25 -0400

    Updated international contact page and sitemap

---
+++ page-templates/content-contact-international.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Template Name: Contact Us - International
+ * Description: Contact us page for the international base site
+ */
+?>
+<!doctype html>
+<html <?php bellhop_lang(); ?>>
+  <?php get_partial('meta/head'); ?>
+  <body <?php body_class('content content--contact'); ?>>
+    <header>
+      <?php
+        get_partial('navigation/simplified'); // Simplified navigation
+        get_partial('hero/general'); // General hero area
+      ?>
+    </header>
+    <main>
+      <?php
+        get_partial('form/quote-request'); // Quote request form block
+        get_partial('media/contact-grid'); // Contact grid
+        get_partial('media/location-map'); // Single-location interactive map
+      ?>
+    </main>
+    <footer class="universal-footer">
+      <?php // Universal footer blocks
+        get_partial('footer/newsletter'); // Newsletter signup form
+        get_partial('footer/masthead'); // Footer masthead
+        get_partial('footer/quicklinks'); // Quicklink buttons
+        get_partial('footer/contacts'); // Contact blocks
+        get_partial('footer/badges'); // Crediibility badges
+        get_partial('footer/copyright'); // Copyright phrasing and links
+      ?>
+    </footer>
+    <?php get_partial('meta/foot'); ?>
+  </body>
+</html>
--- partials/navigation/sitemap.php
+++ partials/navigation/sitemap.php
@@ -1,7 +1,57 @@
 <?php
-// Text-based links to show a map of site pages
+/**
+ * Sitemap
+ *
+ * Text-based links to show a map of site pages, intended to be Human-readable
+ *
+ * @var array $links     Copyright-related links
+ */
+
+if( function_exists('get_field') ):
+  $links = 'footer_links';
+endif;
 ?>
 <section class="o-sitemap">
-  <div class="o-sitemap__content">
-  </div>
+  <h2 class="o-sitemap__headline">Pages</h2>
+  <?php
+    // Displays the navigation menu as list items
+    //https://developer.wordpress.org/reference/functions/wp_nav_menu/
+    $args = array(
+      'menu' => '',
+      'container' => '',
+      'container_class' => '',
+      'container_id' => '',
+      'menu_class' => 'o-sitemap__list',
+      'menu_id' => '',
+      'echo' => true,
+      'fallback_cb' => 'wp_page_menu',
+      'before' => '',
+      'after' => '',
+      'link_before' => '',
+      'link_after' => '',
+      'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+      'item_spacing' => 'preserve',
+      'depth' => 0,
+      'walker' => '',
+      'theme_location' => 'primary_nav'
+    );
+    wp_nav_menu( $args );
+  ?>
+
+  <h2 class="o-sitemap__headline">Other Links</h2>
+  <ul class="o-sitemap__list">
+    <?php // Displays footer links
+      if( have_rows($links, 'option') ):
+        while( have_rows($links, 'option') ): the_row();
+          $label = get_sub_field('label');
+          $url = get_sub_field('url');
+      ?>
+        <li class="menu-item">
+          <a href="<?php echo $url; ?>" target="_blank"><?php echo $label; ?></a>
+        </li>
+      <?php
+        endwhile;
+      endif;
+    ?>
+  </ul>
 </section>
