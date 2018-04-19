077a006c39baf0310a5465ca4996e2d02b798cb0
Ben Davidson <ben@bendavidson.me>
2018-04-19 17:21:15 -0400

    Removed ACFs, added temporary code for testing

--- partials/navigation/primary.php
+++ partials/navigation/primary.php
@@ -6,50 +6,105 @@
  *
  * @var array $siteLogo               The logo for this site
  *
- * @var string $autoclavesLabel       The label for the autoclaves menu tab
- * @var string $autoclavesGrid        The ACF field for grid items in the
- *                                    autoclaves menu tab
- * @var string $autoclavesHeadline    Header text for the link list in the
- *                                    autoclaves menu tab
- * @var string $autoclavesList        ACF of list items in the autoclaves menu
- *                                    tab
- * @var object $autoclavesButtonLink  The linked page for this menu tab button
- * @var string $autoclavesButtonLabel The label text for this menu tab's button
- *
- * @var string $customersLabel        The label for the customers menu tab
- * @var string $customersList         ACF for the link lists in this menu
- * @var object $customersButtonLink   The linked page for this menu tab button
- * @var string $customersButtonLabel  The label text for this menu tab's button
- *
- * @var string $aboutLabel            The label for the 'about us' menu tab
- * @var string $aboutGrid             ACF for the link lists in the about menu
- * @var object $aboutButtonLink       The linked page for this menu tab button
- * @var string $aboutButtonLabel      The label text for this menu tab's button
- *
- * @var string $supportLabel          The label for the support menu tab
- * @var string $supportGrid           ACF for the link lists in this menu
- * @var object $supportButtonLink     The linekd page for this menu tab button
- * @var string $supportButtonLabel    The label text for this menu tab's button
- *
- * @var string $contactLabel          The label for the contact menu tab
- * @var string $contactGrid           ACF for the link list in this menu
- *
  * @var string $sitePhone             The primary contact phone number for the
  *                                    site
  * @var string $intlMenu              The ACF field for referencing
  *                                    international menu settings
  * @var string $contactPage           The URL of the contact page for this site
  */
+
+if( function_exists('get_field') ):
+  $siteLogo = get_field('site_logo', 'option');
+endif;
+
+// Fallback logo if none is set
+if( !$siteLogo ):
+  $siteLogo = array(
+    'alt' => 'Priorclave',
+    'sizes' => array(
+      'preload' => '//via.placeholder.com/64x17?text=logo',
+      '128w' => '//via.placeholder.com/128x33?text=logo',
+      '240w' => '//via.placeholder.com/240x62?text=logo'
+    )
+  );
+endif;
 ?>
 <section class="o-primaryNav">
   <a class="o-primaryNav__logo" href="<?php echo get_site_url(); ?>">
-    <img class="a-navLogo lazyload lazyload--blurUp"
-      alt=""
-      src="<?php placeholder_img(219,57,'text=logo'); ?>"
+    <img class="a-menuLogo lazyload lazyload--blurUp"
+      alt="<?php echo $siteLogo['alt']; ?>"
+      src="<?php echo $siteLogo['sizes']['preload']; ?>"
+      data-sizes="auto"
+      data-srcset="<?php echo $siteLogo['sizes']['preload']; ?> 64w,
+        <?php echo $siteLogo['sizes']['128w']; ?> 65w,
+        <?php echo $siteLogo['sizes']['240w']; ?> 129w
+      "
     />
   </a>
 
   <nav class="o-primaryNav__menu">
+    <ul class="m-navMenu">
+      <li class="m-navMenu__item menu-header">
+        <a id="menuToggle--autoclave" class="m-navMenu__label --active" href="#">Autoclaves</a>
+        <div id="menu--autoclave" class="m-navBlock">
+          <div class="m-navBlock__grid m-navBlock__grid--autoclaves">
+            <?php
+              for( $i=0; $i<10; $i++):
+            ?>
+              <a class="a-navBlock a-navBlock--autoclaves" href="#">
+                <img class="a-navBlock__image" src="//via.placeholder.com/72x106?text=image" />
+                <h3 class="a-navBlock__title">Top-Loading Autoclaves</h3>
+                <h4 class="a-navBlock__description">60-150L</h4>
+              </a>
+            <?php
+              endfor;
+            ?>
+          </div>
+          <div class="m-navBlock__list m-navBlock__list--autoclaves">
+            <div class="a-navList">
+              <h3 class="a-navList__headline">Autoclave Information</h3>
+              <ul class="a-navList__list">
+                <?php
+                  $links = array(
+                    'Autoclave Pricing',
+                    'Autoclave Accessories',
+                    'Autoclave Options',
+                    'Request a Quote',
+                    'FAQ'
+                  );
+                  foreach( $links as $link ):
+                ?>
+                  <li class="a-navItem">
+                    <a class="a-navItem__link" href="#"><?php echo $link; ?></a>
+                  </li>
+                <?php
+                  endforeach;
+                ?>
+              </ul>
+            </div>
+          </div>
+          <div class="m-navBlock__button">
+            <a class="a-menuButton" href="#">
+              <div class="a-menuButton__label">
+                View All Autoclaves
+              </div>
+            </a>
+          </div>
+        </div>
+      </li>
+      <li class="m-navMenu__item menu-header">
+        <a class="m-navMenu__label" href="#">Customers</a>
+      </li>
+      <li class="m-navMenu__item menu-header">
+        <a class="m-navMenu__label" href="#">Why Priorclave?</a>
+      </li>
+      <li class="m-navMenu__item menu-header">
+        <a class="m-navMenu__label" href="#">Support</a>
+      </li>
+      <li class="m-navMenu__item menu-header">
+        <a class="m-navMenu__label" href="#">Contact</a>
+      </li>
+    </ul>
   </nav>
 
   <div class="o-primaryNav__quicklinks">
