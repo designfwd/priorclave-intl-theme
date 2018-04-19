aaf0f483a93853a36d3fe2b96d6d48df3fc31cfd
Ben Davidson <ben@bendavidson.me>
2018-04-19 10:58:51 -0400

    Added header comments for navigation ACFs

--- partials/navigation/primary.php
+++ partials/navigation/primary.php
@@ -1,32 +1,67 @@
 <?php
-// Primary navigation system
+/**
+ * Primary navigation system
+ *
+ * Logo, dropdown menus, and quicklinks for easy access via a fixed section
+ *
+ * @var array $siteLogo               The logo for this site
+ *
+ * @var string $autoclavesLabel       The label for the autoclaves menu tab
+ * @var string $autoclavesGrid        The ACF field for grid items in the
+ *                                    autoclaves menu tab
+ * @var string $autoclavesHeadline    Header text for the link list in the
+ *                                    autoclaves menu tab
+ * @var string $autoclavesList        ACF of list items in the autoclaves menu
+ *                                    tab
+ * @var object $autoclavesButtonLink  The linked page for this menu tab button
+ * @var string $autoclavesButtonLabel The label text for this menu tab's button
+ *
+ * @var string $customersLabel        The label for the customers menu tab
+ * @var string $customersList         ACF for the link lists in this menu
+ * @var object $customersButtonLink   The linked page for this menu tab button
+ * @var string $customersButtonLabel  The label text for this menu tab's button
+ *
+ * @var string $aboutLabel            The label for the 'about us' menu tab
+ * @var string $aboutGrid             ACF for the link lists in the about menu
+ * @var object $aboutButtonLink       The linked page for this menu tab button
+ * @var string $aboutButtonLabel      The label text for this menu tab's button
+ *
+ * @var string $supportLabel          The label for the support menu tab
+ * @var string $supportGrid           ACF for the link lists in this menu
+ * @var object $supportButtonLink     The linekd page for this menu tab button
+ * @var string $supportButtonLabel    The label text for this menu tab's button
+ *
+ * @var string $contactLabel          The label for the contact menu tab
+ * @var string $contactGrid           ACF for the link list in this menu
+ *
+ * @var string $sitePhone             The primary contact phone number for the
+ *                                    site
+ * @var string $intlMenu              The ACF field for referencing
+ *                                    international menu settings
+ * @var string $contactPage           The URL of the contact page for this site
+ */
 ?>
 <section class="o-primaryNav">
-  <a href="#">
-    <img class="o-primaryNav__logo" src="<?php placeholder_img(219,57,'text=logo'); ?>" />
+  <a class="o-primaryNav__logo" href="<?php echo get_site_url(); ?>">
+    <img class="a-navLogo lazyload lazyload--blurUp"
+      alt=""
+      src="<?php placeholder_img(219,57,'text=logo'); ?>"
+    />
   </a>
 
   <nav class="o-primaryNav__menu">
-    <?php
-      wp_nav_menu(
-        array(
-          'menu' => 'Primary Navigation',
-          'menu_class' => 'm-primaryNav',
-          'walker' => new primaryNavWalker,
-        )
-      );
-    ?>
   </nav>
 
   <div class="o-primaryNav__quicklinks">
-      <a class="" href="#">CALL +12 34 5678 9012</a>
-      <a class="" href="#">
+    <div class="m-navQuicklinks">
+      <a class="m-navQuicklinks__phone" href="#">CALL +12 34 5678 9012</a>
+      <a class="m-navQuicklinks__menu" href="#">
         <svg id="" class="a-dropdownLink a-dropdownLink--icon" viewBox="0 0 640 480">
           <?php get_svg('flag/gb'); ?>
         </svg>
       </a>
 
-      <a href="#">
+      <a class="m-navQuicklinks__search" href="#">
         <svg class="a-navIcon" viewBox="0 0 16 16">
           <?php get_svg('icon-search'); ?>
         </svg>
@@ -34,5 +69,6 @@
       <a class="a-navButton a-navButton--primary" href="#">
         Request Quote
       </a>
+    </div>
   </div>
 </section>
