c2e374fa8365586644e2e2fa800540955bdc8731
Ben Davidson <ben@bendavidson.me>
2018-04-24 16:34:56 -0400

    Added ACF integration for product features module

--- partials/content/product-features.php
+++ partials/content/product-features.php
@@ -1,5 +1,25 @@
 <?php
-// Content block with product description and standard features
+/**
+ * Product features section
+ *
+ * Content block with product description and standard features
+ *
+ * @var string $description       Short description of this autoclave type
+ * @var array $models             The list of autoclave models associated with
+ *                                this autoclave type
+ * @var string $extras            ACF associated with extra info blocks, such
+ *                                as heating source and vacuum/dry
+ * @var string $features          ACF to reference list of standard features
+ *
+ */
+
+if( function_exists('get_field') ):
+  $ID = get_the_ID();
+  $description = get_field('content_productFeatures_description', $ID);
+  $models = get_field('content_productFeatures_models', $ID);
+  $extras = 'content_productFeatures_extras';
+  $features = 'content_productFeatures_features';
+endif;
 ?>
 <section id="productFeatures" class="o-productFeatures">
 
@@ -9,53 +29,60 @@
         Product Description
       </h2>
       <div class="m-productDescription__description">
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-        </p>
+        <?php echo $description; ?>
       </div>
       <div class="m-productDescription__sections">
-        <div class="a-productSection">
-          <h3 class="a-productSection__headline">
-            Models
-          </h3>
-          <ul class="a-productSection__list">
-            <li class="a-productSection__item">
-              Model: BTSD 40 Liter Capacity
-            </li>
-            <li class="a-productSection__item">
-              Model: BTSD 40 Liter Capacity
-            </li>
-            <li class="a-productSection__item">
-              Model: BTSD 60 Liter Capacity
-            </li>
-            <li class="a-productSection__item">
-              Model: BTSD 60 Liter Capacity
-            </li>
-          </ul>
-        </div>
-        <div class="a-productSection">
-          <h3 class="a-productSection__headline">
-            Heat Source
-          </h3>
-          <ul class="a-productSection__list">
-            <li class="a-productSection__item">
-              Electrical available at no extra cost
-            </li>
-          </ul>
-        </div>
-        <div class="a-productSection">
-          <h3 class="a-productSection__headline">
-            Vacuum & Dry
-          </h3>
-          <ul class="a-productSection__list">
-            <li class="a-productSection__item">
-              Vacuum available as option
-            </li>
-            <li class="a-productSection__item">
-              Drying available as option
-            </li>
-          </ul>
-        </div>
+        <?php
+        if( $models ):
+        ?>
+          <div class="a-productSection">
+            <h3 class="a-productSection__headline">
+              Models
+            </h3>
+            <ul class="a-productSection__list">
+              <?php
+              foreach( $models as $post ):
+                setup_postdata($post);
+                $headline = get_field('autoclave_model', $post->ID);
+                ?>
+                <li class="a-productSection__item">
+                  Model: <?php echo $headline; ?>
+                </li>
+                <?php
+              endforeach;
+              wp_reset_postdata();
+              ?>
+            </ul>
+          </div>
+        <?php
+        endif;
+
+        if( have_rows($extras, $ID) ):
+          while( have_rows($extras, $ID) ): the_row();
+            $headline = get_sub_field('headline');
+            $items = 'items'; // Nested repeater
+          ?>
+            <div class="a-productSection">
+              <h3 class="a-productSection__headline">
+                <?php echo $headline; ?>
+              </h3>
+              <ul class="a-productSection__list">
+                <?php
+                while( have_rows( $items ) ): the_row();
+                  $item = get_sub_field('item');
+                ?>
+                  <li class="a-productSection__item">
+                    <?php echo $item; ?>
+                  </li>
+                <?php
+                endwhile;
+                ?>
+              </ul>
+            </div>
+          <?php
+          endwhile;
+        endif;
+        ?>
       </div>
     </div>
   </div>
@@ -67,23 +94,17 @@
       </h2>
       <ul class="m-productStandards__list">
         <?php // Standard Features list
-          $features = array(
-            'Flexible TACTROL® 2 Control System',
-            'Multi-Program Memory (5 & 10 programs available)',
-            'Vacuum, Drying, Heating options available',
-            'One-Button Start, Media Warmin, Delayed Start',
-            'Biomaster® Antimicrobial Services',
-            '316L ASME-Certified Stainless Steel Chamber & Door',
-            '2-Year Warranty on Installation, Maintenance, Parts',
-          );
-          foreach( $features as $feature ):
-        ?>
-          <li class="m-productStandards__item">
-            <svg class="a-inlineIcon" viewBox="0 0 16 16"><?php get_svg('icon-check-circle'); ?></svg>
-            <?php echo $feature; ?>
-          </li>
-        <?php
-          endforeach;
+        if( have_rows($features, $ID) ):
+          while( have_rows($features, $ID) ): the_row();
+            $feature = get_sub_field('feature');
+          ?>
+            <li class="m-productStandards__item">
+              <svg class="a-inlineIcon" viewBox="0 0 16 16"><?php get_svg('icon-check-circle'); ?></svg>
+              <?php echo $feature; ?>
+            </li>
+          <?php
+          endwhile;
+        endif;
         ?>
       </ul>
     </div>
