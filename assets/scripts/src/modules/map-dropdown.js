f05e1be6f0c72ae0f754a09ab05e8fb65f9ef869
Ben Davidson <ben@bendavidson.me>
2018-07-11 11:14:08 -0400

    Removed default anchor behavior

--- assets/scripts/src/modules/map-dropdown.js
+++ assets/scripts/src/modules/map-dropdown.js
@@ -1,6 +1,7 @@
 // Handles URL switching based on dropdown options on the multiregion homepage map
 jQuery(document).ready(function() {
-  jQuery('#homepageMap-dropdownMenu').click(function() {
+  jQuery('#homepageMap-dropdownMenu').click(function(e) {
+    e.preventDefault();
     jQuery('#homepageMap-toggleIcon').toggleClass('a-dropdownLink--icon--toggled');
     jQuery('#homepageMap-dropdownList').fadeToggle();
   });
