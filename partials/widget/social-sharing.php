ff9240591bfeeb361a6f5b9b164ddb27d9b7c58d
Ben Davidson <ben@bendavidson.me>
2018-07-27 17:07:08 -0400

    Updated LinkedIn link

--- partials/widget/social-sharing.php
+++ partials/widget/social-sharing.php
@@ -31,7 +31,7 @@
   <div class="o-sidebarSocial__button o-sidebarSocial__button--linkedIn" href="<?php echo $linkedInLink; ?>" target="_blank">
     <a class="a-socialIcon" href="<?php echo $linkedInLink; ?>" target="_blank">
       <svg class="a-socialIcon__svg" viewBox="0 0 16 16">
-        <?php get_svg('social-linkedin'); ?>
+        <?php get_svg('social-linkedIn'); ?>
       </svg>
     </a>
   </div>
