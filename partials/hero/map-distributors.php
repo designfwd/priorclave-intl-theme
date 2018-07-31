02fa42c9ccf7111347bbc98a4d74ea17fa793475
Ben Davidson <ben@bendavidson.me>
2018-07-31 16:21:35 -0400

    Added comment block

--- partials/hero/map-distributors.php
+++ partials/hero/map-distributors.php
@@ -1,6 +1,17 @@
 <?php
-// Interactive Google map of autoclave distributors
+/**
+ * Interactive Google map of autoclave distributors
+ *
+ * Interactive map, using Google Maps plugin, to display worldwide autoclave
+ * distributors and their locations.
+ *
+ * @param string $map     The ID of the map to use
+ */
+
+if( function_exists('get_field') ):
+  $map = get_field('hero_mapDistributors_id');
+endif;
 ?>
 <section class="o-distributorsMap">
-  
+<?php var_dump($map); ?>
 </section>
