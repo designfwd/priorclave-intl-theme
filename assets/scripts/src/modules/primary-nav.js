bf77f6f1a09cfd5c56c09c5d2ba63aa6dc71daf9
Ben Davidson <ben@bendavidson.me>
2018-09-29 21:45:28 -0400

    Added mobile menu and country selector actions

--- assets/scripts/src/modules/primary-nav.js
+++ assets/scripts/src/modules/primary-nav.js
@@ -1,10 +1,38 @@
 // Primary navigation scripts
 jQuery(document).ready(function() {
   //Preload removal and initial hiding
+  jQuery('.o-mobileMenu').hide();
+  jQuery('.o-mobileMenu').addClass('o-mobileMenu--has-loaded');
+  jQuery('.o-mobileMenu').removeClass('o-mobileMenu--preload');
   jQuery('.m-navBlock').hide();
-  jQuery('.m-navBlock').addClass('--has-loaded');
-  jQuery('.m-navBlock').removeClass('--preload');
-  jQuery('.m-navMenu__label').addClass('--has-loaded');
+  jQuery('.m-navBlock').addClass('m-navBlock--has-loaded');
+  jQuery('.m-navBlock').removeClass('m-navBlock--preload');
+  jQuery('.m-navMenu__label').addClass('m-navBlock--has-loaded');
+  jQuery('.m-countrySelector').hide();
+  jQuery('.m-countrySelector').addClass('m-countrySelector--has-loaded');
+  jQuery('.m-countrySelector').removeClass('m-countrySelector--preload');
+
+  jQuery('#mobile-navigation-toggle').click(function() {
+    // If the menu is open, close it
+    if( jQuery(this).hasClass('--open') ){
+      jQuery('.o-mobileMenu').slideToggle();
+      jQuery(this).removeClass('--open');
+    } else {
+      jQuery(this).addClass('--open');
+      jQuery('.o-mobileMenu').slideToggle();
+    }
+  });
+
+  jQuery('#mobile-country-toggle').click(function() {
+    //If the menu is open, close it
+    if( jQuery(this).hasClass('--open') ){
+      jQuery('.m-countrySelector').slideToggle();
+      jQuery(this).removeClass('--open');
+    } else {
+      jQuery(this).addClass('--open');
+      jQuery('.m-countrySelector').slideToggle();
+    }
+  })
 
   jQuery('.m-navMenu__label').click(function() {
     // If the menu is open, close it
