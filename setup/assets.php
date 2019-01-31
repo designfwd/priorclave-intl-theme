036ca6e5582f861555a1d19fd348965d4bfddf1f
benjatron <ben@bendavidson.me>
2019-01-31 15:17:56 -0500

    Added cookie-consent loading

--- setup/assets.php
+++ setup/assets.php
@@ -4,6 +4,7 @@ function priorclave_asset_loader()
 {
   // Styles
   wp_enqueue_style('main', get_stylesheet_directory_uri() . '/assets/styles/dist/main.css', false, null);
+  wp_enqueue_style( 'cookie-consent', '//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css', false, '3.1.0');
 
   // Scripts
   wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/scripts/dist/main.js', array(), null, true);
