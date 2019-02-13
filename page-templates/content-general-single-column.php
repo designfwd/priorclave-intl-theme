4c1566742e294e5e2561022c9415e0b073a0ac76
benjatron <ben@bendavidson.me>
2019-02-13 15:42:00 -0500

    Created single page content page template

---
+++ page-templates/content-general-single-column.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Content - General (Single Column)
+ * Description: Single column general content page
+ */
+?>
+<!doctype html>
+<html <?php priorclave_lang(); ?>>
+  <?php get_partial('meta/head'); ?>
+  <body <?php body_class('content content--general'); ?>>
+    <header>
+      <?php
+        if( get_current_blog_id() == 1 ):
+          get_partial('navigation/simplified'); // Multisite navigation
+        else:
+          get_partial('navigation/primary'); // Primary navigation
+        endif;
+        get_partial('hero/general'); // General hero area
+      ?>
+    </header>
+    <main class="content__main">
+      <?php
+        get_partial('content/general-block'); // Page body content
+      ?>
+    </main>
+    <footer class="universal-footer">
+      <?php
+        // Universal footer blocks
+        get_partial('footer/newsletter'); // Newsletter signup form
+        get_partial('footer/masthead'); // Footer masthead
+        get_partial('footer/quicklinks'); // Quicklink buttons
+        get_partial('footer/contacts'); // Contact blocks
+        get_partial('footer/badges'); // Crediibility badges
+        get_partial('footer/copyright'); // Copyright phrasing and links
+      ?>
+    </footer>
+    <?php get_partial('meta/foot'); ?>
+  </body>
+</html>
