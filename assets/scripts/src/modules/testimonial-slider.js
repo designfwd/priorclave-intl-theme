aa88d99ab27b7bbf4a2fcc84b047a30b1ecd80c6
benjatron <ben@bendavidson.me>
2019-02-14 16:13:56 -0500

    Added slider settings

--- assets/scripts/src/modules/testimonial-slider.js
+++ assets/scripts/src/modules/testimonial-slider.js
@@ -2,7 +2,9 @@
 jQuery(document).ready(function() {
   jQuery('.m-testimonialSlide').removeClass('--preload');
   jQuery('.o-testimonialSlider__slides').slick({
+    autoplay: true,
+    autoplaySpeed: 7000,
     dots: true,
-    speed: 500,
+    speed: 1000,
   });
 });
