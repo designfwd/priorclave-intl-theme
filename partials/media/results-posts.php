271ade38509eee201738798c7855d96d881771d9
benjatron <ben@bendavidson.me>
2019-02-15 09:50:44 -0500

    Updated image representation

--- partials/media/results-posts.php
+++ partials/media/results-posts.php
@@ -4,17 +4,17 @@ $search = get_query_var( 'search' );
 ?>
 <section class="o-articleGrid">
   <h2 class="o-articleGrid__headline">
-    Search Results for "<?php the_search_query(); ?>" 
+    Search Results for "<?php the_search_query(); ?>"
   </h2>
   <?php
-    if($search->have_posts()) : 
+    if($search->have_posts()) :
       while($search->have_posts()) : $search->the_post();
   ?>
     <div class="o-articleGrid__article">
       <div class="m-articlePreview m-articlePreview--blog" data-href="<?php the_permalink(); ?>">
           <img class="m-articlePreview__featuredImage lazyload" src="<?php the_post_thumbnail_url('640w'); ?>" />
-          <img class="m-articlePreview__photo lazyload" src="<?php get_picsum(72,72); ?>" />
-          <h3 class="m-articlePreview__headline" href="#"><?php the_title(); ?></h3>
+          <img class="m-articlePreview__photo lazyload" src="<?php echo get_avatar_url(get_the_ID(), array('size'=>72)); ?>" />
+          <h3 class="m-articlePreview__headline" href="<?php echo the_permalink(); ?>"><?php the_title(); ?></h3>
           <div class="m-articlePreview__article">
             <?php the_excerpt(); ?>
           </div>
