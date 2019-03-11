18bd4f1320647fe502189a195fe76d9a33b8e302
benjatron <ben@bendavidson.me>
2019-03-11 17:30:04 -0400

    Created customers page template

---
+++ page-templates/content-customers.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Content - Customers
+ * Description: Parent template for general content pages
+ */
+?>
+<!doctype html>
+<html <?php priorclave_lang(); ?>>
+  <?php get_partial('meta/head'); ?>
+  <body <?php body_class('content content--category'); ?>>
+    <header>
+      <?php
+        get_partial('navigation/primary'); // Primary navigation
+        get_partial('hero/general'); // General hero area
+      ?>
+    </header>
+    <main>
+      <?php
+        get_partial('content/intro-inverted'); // Intro content block
+        get_partial('navigation/link-grid-group'); // Grid of headlined links
+        get_partial('content/feature-block'); // Feature content block
+        get_partial('media/featured-posts'); // Featured posts
+        get_partial('media/slider-testimonial'); // Testimonial slider
+      ?>
+    </main>
+    <footer class="universal-footer">
+      <?php // Universal footer blocks
+        get_partial('footer/newsletter'); // Newsletter signup form
+        get_partial('footer/masthead'); // Footer masthead
+        get_partial('footer/quicklinks'); // Quicklink buttons
+        get_partial('footer/contacts'); // Contact blocks
+        get_partial('footer/badges'); // Crediibility badges
+        get_partial('footer/copyright'); // Copyright phrasing and links
+      ?>
+    </footer>
+    <?php get_partial('meta/foot'); ?>
+  </body>
+</html>
