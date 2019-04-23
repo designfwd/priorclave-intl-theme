ffb9aff4a6583b88e824b470cbb3f885bde620c5
benjatron <ben@bendavidson.me>
2019-04-23 13:48:51 -0400

    Updated video grid styles

--- partials/media/video-grid.php
+++ partials/media/video-grid.php
@@ -1,5 +1,16 @@
 <?php
 // Grid of video previews
+/**
+ * Video grid
+ *
+ * A grid layout of video previews, which open larger video players on click
+ *
+ * @var array $videos      Video information array
+ */
+
+// Sets variable
+$videos = get_field('media_videoGallery_videos');
+var_dump($videos);
 ?>
 <section class="o-videoGrid">
   <?php
