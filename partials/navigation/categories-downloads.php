eebbccd5946418998982c0f8ff093830be8883b3
sajison <saji@designfwd.com>
2019-06-06 16:15:42 -0400

    duplicating faq pages for downloads

---
+++ partials/navigation/categories-downloads.php
@@ -0,0 +1,28 @@
+<?php
+// A list of category links arranged in blocks
+/**
+ * Blocks of linked FAQ subjects
+ *
+ * A list of the FAQ subject taxonomy terns, set in the admin and displayed in
+ * a set order as link blocks
+ *
+ * @param array $subjects     The FAQ subjects to display
+ */
+
+if( function_exists('get_field') ):
+  $subjects = get_field('navigation_categoriesFAQ_subjects');
+endif;
+?>
+<section class="o-categoryNav o-categoryNav--faq">
+  <?php
+    foreach( $subjects as $subject ):
+  ?>
+    <a class="o-categoryNav__link o-categoryNav__link--faq" href="#faq-<?php echo $subject->slug; ?>">
+      <div class="o-categoryNav__block o-categoryNav__block--faq">
+        <?php echo $subject->name; ?>
+      </div>
+    </a>
+  <?php
+    endforeach;
+  ?>
+</section>
