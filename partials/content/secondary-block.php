be02c2b909363ae5671bcfd7cf56a4e9669de797
Ben Davidson <ben@bendavidson.me>
2020-05-18 11:40:04 -0400

    Updated default/fallback text

--- partials/content/secondary-block.php
+++ partials/content/secondary-block.php
@@ -11,12 +11,13 @@ if( function_exists('get_field') ):
   $ID = get_the_ID();
   $content = get_field('content_secondaryBlock_content', $ID);
 endif;
-if( !isset($content) ):
-  $content = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
-endif;
+
+if( $content ):
 ?>
 <section class="o-secondaryBlock">
   <div class="o-secondaryBlock__content">
     <?php echo $content; ?>
   </div>
 </section>
+<?php
+endif;
